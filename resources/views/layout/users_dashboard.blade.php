35fbd13f3544952ce04fe9e834609cd64fb39f8b
=chriss-inno <chriss.innocent@outlook.com>
2015-10-26 18:33:13 +0300

    Update framework

--- resources/views/layout/users_dashboard.blade.php
+++ resources/views/layout/users_dashboard.blade.php
@@ -399,119 +399,120 @@
                         <h3 class="text-info"> <strong><i class="fa  fa-user"></i> WELCOME <span class="text-danger">{{strtoupper($user->first_name.' '.$user->last_name)}} </span></strong></h3>
                     </header>
                     <div class="panel-body">
-
-                        @if (count($errors) > 0)
-                            <div class="alert alert-danger">
-                                <ul>
-                                    @foreach ($errors->all() as $error)
-                                        <li>{{ $error }}</li>
-                                    @endforeach
-                                </ul>
-                            </div>
-                        @endif
-
-                        <fieldset class="scheduler-border">
-                            <legend class="scheduler-border" style="color:#005DAD">Personal details</legend>
-                            <div class="form-group">
-                                <div class="row">
-                                    <div class="col-md-4">
-                                        <label for="first_name">First Name</label>
-                                        <input type="text" class="form-control" id="first_name" name="first_name" placeholder="Enter First Name" required autocomplete=off @if(old('first_name') !="")value="{{old('first_name')}}" @else value="{{$user->first_name}}" @endif readonly>
-                                        @if($errors->first('first_name'))
-                                            <label for="first_name" class="error">{{$errors->first('first_name')}}</label>
-                                        @endif
-                                    </div>
-                                    <div class="col-md-4">
-                                        <label for="last_name">Last Name</label>
-                                        <input type="text" class="form-control" id="last_name" name="last_name" placeholder="Enter Last Name" required autocomplete=off @if(old('last_name') !="")value="{{old('last_name')}}" @else value="{{$user->last_name}}" @endif readonly>
-                                        @if($errors->first('last_name'))
-                                            <label for="first_name" class="error">{{$errors->first('last_name')}}</label>
-                                        @endif
-                                    </div>
-                                    <div class="col-md-4">
-                                        <label for="middle_name">Other Name</label>
-                                        <input type="text" class="form-control" id="middle_name" name="middle_name" placeholder="Enter Middle Name" @if(old('middle_name') !="")value="{{old('middle_name')}}" @else value="{{$user->middle_name}}" @endif readonly>
-                                    </div>
+                        <div class="row">
+                            <div class="col-lg-9 col-md-9 col-sm-9 col-xs-9">
+                            @if (count($errors) > 0)
+                                <div class="alert alert-danger">
+                                    <ul>
+                                        @foreach ($errors->all() as $error)
+                                            <li>{{ $error }}</li>
+                                        @endforeach
+                                    </ul>
                                 </div>
+                            @endif
 
-                            </div>
-
-                            <div class="form-group">
-                                <label for="designation">Designation</label>
-                                <input type="text" class="form-control" id="designation " name="designation" placeholder="Enter Designation" required @if(old('designation') !="")value="{{old('designation')}}" @else value="{{$user->designation}}" @endif readonly>
-                                @if($errors->first('designation'))
-                                    <label for="first_name" class="error">{{$errors->first('designation')}}</label>
-                                @endif
-                                <p class="help-block">Please enter full details of your designation, do not enter abbreviation.</p>
-                            </div>
-                            <div class="form-group">
-                                <div class="row">
-                                    <div class="col-md-4">
-                                        <label for="phone">Mobile Number</label>
-                                        <input type="text" class="form-control"  id="phone" name="phone" placeholder="Enter Mobile Number" autocomplete=off @if(old('phone') !="") value="{{old('phone')}}" @else value="{{$user->phone}} "@endif readonly>
-                                        @if($errors->first('phone'))
-                                            <label for="first_name" class="error">{{$errors->first('phone')}}</label>
-                                        @endif
-                                    </div>
-                                    <div class="col-md-8">
-                                        <label for="email">E-Mail</label>
-                                        <input type="email" class="form-control"  id="email" name="email" placeholder="Enter email address" autocomplete=off @if(old('email') !="") value="{{old('email')}}" @else value="{{$user->email}}" @endif readonly>
-                                        @if($errors->first('email'))
-                                            <label for="first_name" class="error">{{$errors->first('email')}}</label>
-                                        @endif
-                                    </div>
-                                </div>
-                            </div>
-
-                        </fieldset>
-
-                        <fieldset class="scheduler-border">
-                            <legend class="scheduler-border" style="color:#005DAD">Department details</legend>
-                            <div class="form-group">
-                                <div class="row">
-                                    <div class="col-md-6">
-                                        <label for="branchusr">Branch</label>
-                                        <select class="form-control"  id="branchusr" name="branchusr" disabled>
-                                            <?php $branches=\App\Branch::all();?>
-
-                                            @if(old('branch') !="")
-                                                <?php $branchd=\App\Branch::find(old('branch'));?>
-                                                <option value="{{$branchd->id}}" selected>{{$branchd->branch_Name}}</option>
-                                            @else
-                                                <?php $branchd=\App\Branch::find($user->branch_id);?>
-                                                <option value="{{$branchd->id}}" selected>{{$branchd->branch_Name}}</option>
+                            <fieldset class="scheduler-border">
+                                <legend class="scheduler-border" style="color:#005DAD">Personal details</legend>
+                                <div class="form-group">
+                                    <div class="row">
+                                        <div class="col-md-4">
+                                            <label for="first_name">First Name</label>
+                                            <input type="text" class="form-control" id="first_name" name="first_name" placeholder="Enter First Name" required autocomplete=off @if(old('first_name') !="")value="{{old('first_name')}}" @else value="{{$user->first_name}}" @endif readonly>
+                                            @if($errors->first('first_name'))
+                                                <label for="first_name" class="error">{{$errors->first('first_name')}}</label>
                                             @endif
-                                            <?php $branches=\App\Branch::all();?>
-                                            @foreach($branches as $br)
-                                                <option value="{{$br->id}}">{{$br->branch_Name}}</option>
-                                            @endforeach
-
-                                        </select>
-                                        @if($errors->first('branchusr'))
-                                            <label for="branchusr" class="error">{{$errors->first('branchusr')}}</label>
-                                        @endif
-                                    </div>
-                                    <div class="col-md-6">
-                                        <label for="departmentUsr">Department</label>
-                                        <select class="form-control"  id="departmentUsr" name="departmentUsr" disabled>
-                                            @if(old('branch') !="")
-                                                <?php $depart=\App\Department::find(old('department'));?>
-                                                <option value="{{$depart->id}}" selected>{{$depart->department_name}}</option>
-                                            @else
-                                                <?php $depart=\App\Department::find($user->department_id);?>
-                                                <option value="{{$depart->id}}" selected>{{$depart->department_name}}</option>
+                                        </div>
+                                        <div class="col-md-4">
+                                            <label for="last_name">Last Name</label>
+                                            <input type="text" class="form-control" id="last_name" name="last_name" placeholder="Enter Last Name" required autocomplete=off @if(old('last_name') !="")value="{{old('last_name')}}" @else value="{{$user->last_name}}" @endif readonly>
+                                            @if($errors->first('last_name'))
+                                                <label for="first_name" class="error">{{$errors->first('last_name')}}</label>
                                             @endif
-                                        </select>
-                                        @if($errors->first('departmentUsr'))
-                                            <label for="department" class="error">{{$errors->first('departmentUsr')}}</label>
-                                        @endif
+                                        </div>
+                                        <div class="col-md-4">
+                                            <label for="middle_name">Other Name</label>
+                                            <input type="text" class="form-control" id="middle_name" name="middle_name" placeholder="Enter Middle Name" @if(old('middle_name') !="")value="{{old('middle_name')}}" @else value="{{$user->middle_name}}" @endif readonly>
+                                        </div>
+                                    </div>
+
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="designation">Designation</label>
+                                    <input type="text" class="form-control" id="designation " name="designation" placeholder="Enter Designation" required @if(old('designation') !="")value="{{old('designation')}}" @else value="{{$user->designation}}" @endif readonly>
+                                    @if($errors->first('designation'))
+                                        <label for="first_name" class="error">{{$errors->first('designation')}}</label>
+                                    @endif
+                                    <p class="help-block">Please enter full details of your designation, do not enter abbreviation.</p>
+                                </div>
+                                <div class="form-group">
+                                    <div class="row">
+                                        <div class="col-md-4">
+                                            <label for="phone">Mobile Number</label>
+                                            <input type="text" class="form-control"  id="phone" name="phone" placeholder="Enter Mobile Number" autocomplete=off @if(old('phone') !="") value="{{old('phone')}}" @else value="{{$user->phone}} "@endif readonly>
+                                            @if($errors->first('phone'))
+                                                <label for="first_name" class="error">{{$errors->first('phone')}}</label>
+                                            @endif
+                                        </div>
+                                        <div class="col-md-8">
+                                            <label for="email">E-Mail</label>
+                                            <input type="email" class="form-control"  id="email" name="email" placeholder="Enter email address" autocomplete=off @if(old('email') !="") value="{{old('email')}}" @else value="{{$user->email}}" @endif readonly>
+                                            @if($errors->first('email'))
+                                                <label for="first_name" class="error">{{$errors->first('email')}}</label>
+                                            @endif
+                                        </div>
                                     </div>
                                 </div>
+
+                            </fieldset>
+
+                            <fieldset class="scheduler-border">
+                                <legend class="scheduler-border" style="color:#005DAD">Department details</legend>
+                                <div class="form-group">
+                                    <div class="row">
+                                        <div class="col-md-6">
+                                            <label for="branchusr">Branch</label>
+                                            <select class="form-control"  id="branchusr" name="branchusr" disabled>
+                                                <?php $branches=\App\Branch::all();?>
+
+                                                @if(old('branch') !="")
+                                                    <?php $branchd=\App\Branch::find(old('branch'));?>
+                                                    <option value="{{$branchd->id}}" selected>{{$branchd->branch_Name}}</option>
+                                                @else
+                                                    <?php $branchd=\App\Branch::find($user->branch_id);?>
+                                                    <option value="{{$branchd->id}}" selected>{{$branchd->branch_Name}}</option>
+                                                @endif
+                                                <?php $branches=\App\Branch::all();?>
+                                                @foreach($branches as $br)
+                                                    <option value="{{$br->id}}">{{$br->branch_Name}}</option>
+                                                @endforeach
+
+                                            </select>
+                                            @if($errors->first('branchusr'))
+                                                <label for="branchusr" class="error">{{$errors->first('branchusr')}}</label>
+                                            @endif
+                                        </div>
+                                        <div class="col-md-6">
+                                            <label for="departmentUsr">Department</label>
+                                            <select class="form-control"  id="departmentUsr" name="departmentUsr" disabled>
+                                                @if(old('branch') !="")
+                                                    <?php $depart=\App\Department::find(old('department'));?>
+                                                    <option value="{{$depart->id}}" selected>{{$depart->department_name}}</option>
+                                                @else
+                                                    <?php $depart=\App\Department::find($user->department_id);?>
+                                                    <option value="{{$depart->id}}" selected>{{$depart->department_name}}</option>
+                                                @endif
+                                            </select>
+                                            @if($errors->first('departmentUsr'))
+                                                <label for="department" class="error">{{$errors->first('departmentUsr')}}</label>
+                                            @endif
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </fieldset>
                             </div>
-
-                        </fieldset>
-
-
+                        </div>
                     </div>
                 </section>
             </div>
